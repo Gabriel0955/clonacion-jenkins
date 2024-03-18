d388e1aaa2f88731214ec47d1fc43971b4d8ba14
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2024-03-18 00:39:40 -0500

    Update jenkinsgroovy.groovy

--- vars/jenkinsgroovy.groovy
+++ vars/jenkinsgroovy.groovy
@@ -2,16 +2,3 @@
 import groovyx.net.http.RESTClient
 import static groovyx.net.http.ContentType.*
 import org.apache.commons.lang.StringUtils
-
-// Crear un cliente RESTClient para probar la importación
-def client = new RESTClient('https://jsonplaceholder.typicode.com')
-
-// Realizar una solicitud GET a un recurso
-def response = client.get(path: '/posts/1')
-println "Status Code: ${response.status}"
-println "Response Body:"
-println response.data
-
-// Verificar si StringUtils está funcionando
-def str = "Hello, world!"
-println "Is empty: ${StringUtils.isEmpty(str)}"
