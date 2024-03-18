6a3cbd0c22bbbfff82a63da82546a43dd232026a
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2024-03-18 12:11:04 -0500

    Update jenkinsgroovy.groovy

--- vars/jenkinsgroovy.groovy
+++ vars/jenkinsgroovy.groovy
@@ -1,7 +1,3 @@
 import groovyx.net.http.RESTClient
 import static groovyx.net.http.ContentType.*
 import org.apache.commons.lang.StringUtils
-
-def call() {
-    println "Las importaciones funcionan correctamente"
-}
