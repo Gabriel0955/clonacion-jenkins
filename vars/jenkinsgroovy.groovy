39b5593e74d11a7e2df50216e1f45b66b8d75360
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2024-03-18 00:22:02 -0500

    Update jenkinsgroovy.groovy

--- vars/jenkinsgroovy.groovy
+++ vars/jenkinsgroovy.groovy
@@ -1,4 +1,4 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
 import groovyx.net.http.RESTClient
 import static groovyx.net.http.ContentType.*
-import hudson.model.*
 import org.apache.commons.lang.StringUtils
